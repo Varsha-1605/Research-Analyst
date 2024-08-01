4c5e12d98dcc79671ff1d82cd65cd78ea0b9e9e6
Varsha Dewangan <140236223+Varsha-1605@users.noreply.github.com>
2024-08-02 02:59:20 +0530

    Update final_research_analyst.py

--- final_research_analyst.py
+++ final_research_analyst.py
@@ -14,11 +14,12 @@ from logging.handlers import RotatingFileHandler
 from datetime import datetime, timedelta
 import traceback
 from langchain.globals import set_verbose
-from dotenv import load_dotenv
-# Load environment variables
-load_dotenv()
+# from dotenv import load_dotenv
+# # Load environment variables
+# load_dotenv()
 
-
+OPENAI_API_KEY = 'sk-proj-bOVUZGprykH02xjEi69sT3BlbkFJKWUpwmYQX3SgBsh6NVk5'
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 # Set up logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
